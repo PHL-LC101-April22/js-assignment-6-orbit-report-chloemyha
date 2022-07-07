cdb91ea6608ea1f39b842003cb624968544a47b6
chloemyha <tup14113@temple.edu>
2022-07-06 22:30:59 -0400

    2nd

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,31 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Satellite } from '../satellite';
+import { Component, OnInit, Input } from "@angular/core";
+import { Satellite } from "../satellite";
 
 @Component({
-  selector: 'app-orbit-counts',
-  templateUrl: './orbit-counts.component.html',
-  styleUrls: ['./orbit-counts.component.css']
+  selector: "app-orbit-counts",
+  templateUrl: "./orbit-counts.component.html",
+  styleUrls: ["./orbit-counts.component.css"],
 })
 export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
 
-	@Input() satellites: Satellite[];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  private typeWithCounts = {};
 
   countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
-	}
-	return count;
- }
+    let count = 0;
+    if (this.satellites) {
+      for (let i = 0; i < this.satellites.length; i++) {
+        if (this.satellites[i].type === type) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
 
-
+//   private handleTypeCounts() {
+//     this.typeWithCounts = {};
+// 	for (let s in this.satellites) {
+// 		if (this.typeWithCounts[s.type]) == null {
+// 			this.typeWithCounts[s.type] = 1
+// 		} else {
+// 			this.typeWithCounts[s.type] += 1
+// 		}
+// 	}
+// 	return this.typeWithCounts
+//   }
 }
